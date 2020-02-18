5bc458311ba51f972d530ac3467b7d1a70245e97
The Farrier <nkfarrey@gmail.com>
2020-02-18 16:35:01 -0600

    Added time tracking javascript

--- Homework4/Assets/script.js
+++ Homework4/Assets/script.js
@@ -12,8 +12,10 @@ var data = [
 ]
 
 var counter = 0;
+var time = 75;
 var getQuestion = document.querySelector("#question");
 var getOptions = document.querySelector("#answer");
+var getTimer = document.querySelector("#timer");
 
 var displayQuestion = document.createElement("h5");
 displayQuestion.setAttribute("class","card-title")
@@ -22,10 +24,26 @@ getQuestion.appendChild(displayQuestion);
 
 for (var i = 0; i < data[counter].options.length; i++){
     var displayOptions = document.createElement('button');
-    displayOptions.setAttribute("class","btn btn-primary");
+    displayOptions.setAttribute("class","btn btn-primary col-4 m-1");
     displayOptions.textContent = data[counter].options[i];
     getOptions.appendChild(displayOptions);
 
-
 }
+
+getOptions.
+
+function timeTracker() {
+    var timerInterval = setInterval(function() {
+      time--;
+      getTimer.textContent = time;
+  
+      if(time === 0) {
+        clearInterval(timerInterval);
+        gameOver();
+      }
+  
+    }, 1000);
+  }
+  
+
 
