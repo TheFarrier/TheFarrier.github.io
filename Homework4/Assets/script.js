8a61c25b6ea62dafcb3a34e85ebbdafbb4b12306
The Farrier <nkfarrey@gmail.com>
2020-02-18 18:34:23 -0600

    Added right/wrong events on button clicks

--- Homework4/Assets/script.js
+++ Homework4/Assets/script.js
@@ -2,7 +2,7 @@ var data = [
     {
         question : "what is the question?",
         options : ["answer1", "answer2", "answer3", "answer4"],
-        answer : "answer1234",
+        answer : "answer1",
     },
     {
         question : "what is the question?",
@@ -30,8 +30,22 @@ for (var i = 0; i < data[counter].options.length; i++){
 
 }
 
-getOptions.
+// Click event to check for correct answers
+getOptions.addEventListener("click",function(event){
 
+    // If correct, button turns green and loads next question
+    if (event.target.textContent === data[counter].answer) {
+        event.target.setAttribute("class","btn btn-success col-4 m-1");
+    
+    // If wrong, button turns red and reduces timer by 5, does not load next question
+    } else {
+        event.target.setAttribute("class","btn btn-danger col-4 m-1");
+        time -= 5;
+        getTimer.textContent = time;
+    }
+});
+
+// Starts countdown and calls gameOver(); when countdown reaches 0
 function timeTracker() {
     var timerInterval = setInterval(function() {
       time--;
@@ -44,6 +58,10 @@ function timeTracker() {
   
     }, 1000);
   }
-  
 
+  function gameOver(){
+      
+  }
+  
+timeTracker();
 
