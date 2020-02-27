9273d685f8b679278af35867d4aeb2e7248ca535
The Farrier <nkfarrey@gmail.com>
2020-02-26 20:03:13 -0600

    Cleaned code, and added comments

--- 02-Homework/Assets/script.js
+++ 02-Homework/Assets/script.js
@@ -1,6 +1,6 @@
 var currentHour = moment()._d.getHours();
-var currentDay = moment()._d.getDay();
-console.log(moment()._d.toDateString());
+var todaysDate = moment()._d.toDateString();
+console.log(todaysDate);
 
 $(".clearBtn").click(function(event){
   localStorage.clear();
@@ -10,12 +10,12 @@ $(".clearBtn").click(function(event){
 function getSchedule(){
   var schedule = JSON.parse(localStorage.getItem("schedule"));
   var currentDay = localStorage.getItem("date");
-  $("#currentDay").text(moment()._d.toDateString());
+  $("#currentDay").text(todaysDate);
   
-  if (currentDay != moment()._d.toDateString()){
+  if (currentDay != todaysDate){
     //clear the schedule because it is not the same day as the stored schedule
     localStorage.clear();
-    localStorage.setItem("date", moment()._d.toDateString());
+    localStorage.setItem("date", todaysDate);
   }
 
   // Use stored Schedule if one exists
@@ -48,6 +48,7 @@ function saveSchedule(schedule){
 function renderSchedule(schedule){
   $(".container").empty()
 
+  // For each hour in schedule, create a new div row
   schedule.forEach(el => {
     var slot = $("<div>").addClass("timeblock row");
     var hour = $("<div>").addClass("hour col-sm-1").text(el.hour);
@@ -63,35 +64,25 @@ function renderSchedule(schedule){
       text.addClass("future");
     };
 
-    //set click handler for save button
+    //set click handler for each save button
     saveBtn.click(function(event) {
       var getNewText = $(this).siblings("textarea").val();
-      console.log(getNewText);
       var getThisButton = $(this).attr("hour");
-      console.log(getThisButton);
+      // Only changes the description for the hour that matches the button's hour data element
       schedule.forEach(el => {
         if(el.time == getThisButton)
         el.description = getNewText;
       })
+      
       event.stopPropagation();
-      console.log(el.description);
-      // I have a data attribute called hour on each of my buttons
-      // inside the click handler I navigate the tree using .parent() and .children to find my textarea
-      // and I set the hour to the value of the textarea
-      renderSchedule(schedule)
-      saveSchedule(schedule)
+      renderSchedule(schedule);
+      saveSchedule(schedule);
     })
 
 
     slot.append(hour, text, saveBtn);
     $(".container").append(slot);
   });
-  // for each hour in the schedule, create a div with the bootstrap class 'row'
-  // append 3 divs with the bootstrap 'col' classes to the above row 
-  // these keep hour, text, and save button
-  // I color the text column depending on status
-  // $(".container").append($("<div>your row goes here<button class='save-btn'>save</button></div>"))
-  // set on click handler for each save button
   
 }
 
