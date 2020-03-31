fdc8afba889de1a3d7a0238cd079f236bd817b76
The Farrier <nkfarrey@gmail.com>
2020-03-30 21:37:31 -0500

    Move test changes from appcopy to app,  delet copy

--- Homework10/Develop/app.js
+++ Homework10/Develop/app.js
@@ -1,10 +1,15 @@
 const fs = require("fs");
+const path = require("path");
+const html = require("./lib/htmlRenderer")
 const inquirer = require("inquirer");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
 // Inquirer question array
+
 let id = 0
 const employees = [];
 const questions = [
@@ -36,69 +41,81 @@ const questions = [
   },
 ];
 
+
 // Use inquirer to ask how many employees you need to add
-async function generateEmployee(){
-  for (var i =0; i<2; i++) { 
-    try {
-      var newEmp = {};
-      const name = await inquirer.prompt(questions[0]);
-      const email = await inquirer.prompt(questions[1]);
-      const role = await inquirer.prompt(questions[2]);
-      id++;
-  
-      switch (role.role){
-        case "Manager":
-          const office = await inquirer.prompt(questions[4])
-          newEmp = new Manager(name,id,email,office)
-          break;
-  
-        case "Engineer":
-          const github = await inquirer.prompt(questions[3])
-          newEmp = new Engineer(name,id,email,github)
-          break;
-        case "Intern":
-          const school = await inquirer.prompt(questions[5])
-          newEmp = new Intern(name,id,email,school)
-          break;
-        default:
-          console.log("Role invalid");
-      }
-      
-      // async function assignRole(role) {
-      //   console.log(role)
-      //   if(role == "Engineer"){
-      //     const github = await inquirer.prompt(questions[3])
-      //     return new Engineer(name, id, email,github);
-      //   }
-      //   if(role == "Manager"){
-      //     const office = await inquirer.prompt(questions[4])
-      //     return new Manager(name, id, email,office);
-      //   }
-      //   if(role == "Intern"){
-      //     const school = await inquirer.prompt(questions[5])
-      //     return new Intern(name, id, email,school);
-      //   } else {
-      //     console.log("Role invalid");
-      //   }
-      // }
-      for (var key in newEmp){
-        console.log(newEmp[key]);
-      }
-      employees.push(newEmp);
-      }
-      
-    catch (err) {
-      console.log(err);
+async function generateEmployee(){ 
+  try {
+    var newEmp = {};
+    const {name} = await inquirer.prompt(questions[0]);
+    const {email} = await inquirer.prompt(questions[1]);
+    const {role} = await inquirer.prompt(questions[2]);
+    id++;
+
+    switch (role){
+      case "Manager":
+        const {office} = await inquirer.prompt(questions[4])
+        newEmp = new Manager(name,id,email,office)
+        break;
+
+      case "Engineer":
+        const {github} = await inquirer.prompt(questions[3])
+        newEmp = new Engineer(name,id,email,github)
+        break;
+      case "Intern":
+        const {school} = await inquirer.prompt(questions[5])
+        newEmp = new Intern(name,id,email,school)
+        break;
+      default:
+        console.log("Role invalid");
     }
+    employees.push(newEmp);
+    await init();
   }
-  
+    
+  catch (err) {
+    console.log(err);
+  }
 };
-// Inquirer loop that asks for employee information
-// role
-// name
-// id
-// email
-// github if engineer
-// office number if manager
-// school if student
-generateEmployee().then(()=> console.log(employees));
+
+async function generateHtml(){
+  console.log(employees);
+  fs.writeFile(outputPath, html(employees),function(err) {
+
+    if (err) {
+      return console.log(err);
+    }
+  
+    console.log("Success!");
+  
+  });
+  console.log("Employee HTML created!");
+}
+
+async function init(){
+  const {run} = await inquirer.prompt([
+    {
+      message:"Add new employee?",
+      type:"list",
+      choices:["Yes","No"],
+      name: "run"
+    }
+  ])
+
+  switch (run){
+    case "Yes":
+      generateEmployee();
+      break;
+    case "No":
+      if(employees[0] !== undefined){
+        generateHtml();
+        
+      } else {
+        console.log("No employees to list")
+      };
+      break;
+    default:
+      console.log("Please make a decision")
+  }
+};
+
+init();
