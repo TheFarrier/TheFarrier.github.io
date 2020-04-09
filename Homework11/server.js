a035e99488f7c367f4f9028828d289d4dbe78110
The Farrier <nkfarrey@gmail.com>
2020-04-08 22:45:58 -0500

    moved readfile into get and post separately

--- Homework11/server.js
+++ Homework11/server.js
@@ -10,7 +10,7 @@ var app = express();
 var PORT = 3000;
 var notePath = path.join(__dirname,"/db/db.json");
 
-// Sets up the Express app to handle data parsing
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
